abf3c7c74552e256ed5228a060a92cbc5a1625c2
Evgeniy2008 <chipserso@gmail.com>
2026-04-01 13:44:11 +0300

    fix

--- src/custom-jsx.d.ts
+++ src/custom-jsx.d.ts
@@ -1,5 +1,7 @@
-declare namespace JSX {
-  interface HTMLAttributes<T> {
+import type * as React from 'react'
+
+declare module 'react' {
+  interface HTMLAttributes<T> extends React.HTMLAttributes<T> {
     bis_skin_checked?: string
   }
 }
